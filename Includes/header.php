a40a735c24224fe51f37f7ebe4edabd1483a0bd1
Pavanvid <pavan.bandi10@gmail.com>
2021-09-28 23:34:56 -0400

    req

--- Includes/header.php
+++ Includes/header.php
@@ -11,21 +11,18 @@
   <title>Door-Step <?php echo $title ?></title>
   </head>
   <body>
-  <div class="container">
-  <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Home</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="index.php">HOME</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <div class="navbar-nav mr-auto">
-          <a class="nav-link active" aria-current="page" href="#">Contact Us</a>
-          <a class="nav-link active" aria-current="page" href ="#">About The Site</a>
-
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="contactus.php">Contact Us</a>
+        <a class="nav-link" href="aboutsite.php">About Us</a>
       </div>
     </div>
   </div>
-</div>
 </nav>
 <div class="container">
