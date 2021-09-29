9240316f2de76d48345195fe762699f2f58e1ade
Pavanvid <pavan.bandi10@gmail.com>
2021-09-29 04:45:55 -0400

    Update index.php

--- index.php
+++ index.php
@@ -76,9 +76,6 @@
     <form method="get" action="groceries.php">
     <button type="Submit" name="submit" class="btn btn-primary" type="button">GROCERIES</button>
     </form>
-    <form method="get" action="meat.php">
-    <button type="Submit" name="submit" class="btn btn-primary" type="button">Meat</button>
-    </form>
      <br>
     <br>
     
