076df3057329a86eb13ac3cb9c9a9ef7adcee40c
Pavanvid <pavan.bandi10@gmail.com>
2021-09-28 21:48:06 -0400

    req

--- aboutsite.php
+++ aboutsite.php
@@ -5,10 +5,11 @@
 <br>
 <br>
 <h1 class="text-center">About This Site</h1>
-
+<?php echo "DOOR-STEP DELIVER" ?>
 <p class="lh-1">Present , We are in the era where everything will get within hours , Everything is under control online services.
     we are living in villages having lots of works like farming , daily works etc.We didn't have time to towns and buy the things.
     We are here to provide everything what you need in the means of Groceries , Meat , Medicines and someother things what you need.
 </p>
+
 
 <?php include 'Includes/footer.php' ?>
