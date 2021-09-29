fef659731ea31fda568a87f3f17a3adf3ab5550b
Pavanvid <pavan.bandi10@gmail.com>
2021-09-28 23:17:38 -0400

    req

--- index.php
+++ index.php
@@ -23,9 +23,13 @@
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="contactus.php">Contact Us</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
         </li>
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href ="aboutsite.php">About The Site</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
       </li>
       </ul>
     </div>
