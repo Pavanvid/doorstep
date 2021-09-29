4c69d4aa14e8719b27772479a7bb7dc371f22587
Pavanvid <pavan.bandi10@gmail.com>
2021-09-28 22:07:30 -0400

    req

---
+++ meat.php
@@ -0,0 +1,36 @@
+<?php
+    $title ="Groceries";
+    include 'Includes/header.php';
+    include 'Database/connection.php';
+?>
+
+
+
+</div>
+        <div class="form-group">
+            <label for="Full Name">Name</label>
+            <input required type="text" class="form-control" id="exampleInputName" placeholder="Full Name" name="FullName">
+
+        </div>
+        <div class = "form-group">
+        <label for ="exampleInputEmail">Email Address</label>
+        <input type="email" class="form-control" id="exampleInputEmial" aria-describedby="emailHelp" 
+        placeholder="Enter email" name="Email">
+        <small id="emailHelp" class="form-text text-muted">Optional</small>
+    </div>
+        <div class="form-group"></div>
+        <label for ="exampleInputNumber">Contact Number</label>
+        <input required type="Contact Number" class="form-control" id="exampleInputPassword" placeholder="Contact Number" name="ContactNo">
+    </div>
+    <div class="container">
+    <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Full Address</label>
+        <textarea required class="form-control" id="exampleFormControlTextarea1" rows="3" name="address"></textarea>
+        <small id="Address_validation" class="form-text text-muted">Please mention your pincode</small>
+    </div>
+    
+        <button type="Submit" name="submit" class="btn btn-primary" type="button">Submit</button>
+        
+        </div>
+    </form>
+<?php echo 'Copyrights' .date('Y') . "Web Master" ?>
