07f2a5efa4a53823e80fed8f5647146b77ec8f35
Pavanvid <pavan.bandi10@gmail.com>
2021-09-29 04:02:34 -0400

    Update header.php

--- Includes/header.php
+++ Includes/header.php
@@ -20,7 +20,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav>
+      <div class="navbar-nav">
         <a class="nav-link active" aria-current="page" href="contactus.php">Contact Us</a>
         <a class="nav-link" href="aboutsite.php">About Us</a>
       </div>
