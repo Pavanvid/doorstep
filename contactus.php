8e20b1cb948534bc58da0074dd64ed3315daf261
Pavanvid <pavan.bandi10@gmail.com>
2021-09-28 21:30:11 -0400

    req

--- contactus.php
+++ contactus.php
@@ -1,5 +1,14 @@
 <?php
+    include 'Includes/header.php'
+?>
+
+<?php
         echo "If your are facing any problems in the means of ordering the items";
+        echo '<hr/>';
+        echo '<br/>';
         echo "please class this number:";
+        echo '<br/>';
         echo "XXXXXXXXXX";
 ?>
+
+<?php include 'Includes/footer.php' ?>
