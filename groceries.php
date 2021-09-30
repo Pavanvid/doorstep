ca45f31c99169217968a07256a0863118dadbb52
Pavanvid <pavan.bandi10@gmail.com>
2021-09-29 22:04:19 -0400

    Update groceries.php

--- groceries.php
+++ groceries.php
@@ -4,13 +4,15 @@
     include 'Database/connection.php';
 ?>
 
-<h1 class="text-center"> Welcome To The Grocery Shop </h1>
+<h1 class="text-center"> Welcome To The Grocery Shop  </h1>
 <marquee class='text-danger'>మీ ప్రతి ఆర్డర్ కి 40 రూపాయలు మాత్రమే సర్వీస్ చార్జ్ పడుతుంది.</marquee>
     <form method="post" action="aftrsubmit.php">  
     <div class="mb-3" >
         <label for="exampleFormControlTextarea1" class="form-label">Which Items you need from the Grocery shop</label>
         <textarea required class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="ఇక్కడ, దయచేసి మీకు కావలసిన కిరాణా సామాగ్రిని నమోదు చేయండి" 
         name="grocery_items"></textarea>
+        <small id="emailHelp" class="form-text text-muted">Type in english or telugu</small>
+        <br>
     </div>
 
 
@@ -19,23 +21,26 @@
             <input required type="text" class="form-control" id="exampleInputName" placeholder="Full Name" name="FullName">
 
         </div>
+        <br>
         <div class = "form-group">
         <label for ="exampleInputEmail">Email Address</label>
         <input type="email" class="form-control" id="exampleInputEmial" aria-describedby="emailHelp" 
         placeholder="Enter email" name="Email">
         <small id="emailHelp" class="form-text text-muted">Optional</small>
     </div>
+    <br>
         <div class="form-group"></div>
         <label for ="exampleInputNumber">Contact Number</label>
         <input required type="Contact Number" class="form-control" id="exampleInputPassword" placeholder="Contact Number" name="ContactNo">
     </div>
+    <br>
     <div class="container">
     <div class="mb-3">
         <label for="exampleFormControlTextarea1" class="form-label">Full Address</label>
         <textarea required class="form-control" id="exampleFormControlTextarea1" rows="3" name="address"></textarea>
         <small id="Address_validation" class="form-text text-muted">Please mention your pincode</small>
     </div>
-    
+    <br>
         <button type="Submit" name="submit" class="btn btn-primary" type="button">Submit</button>
         
         </div>
